72030721466eafa43bcc194d63c5452161ee7292
shelly adiristy <shellyadiristy@gmail.com>
2022-09-17 09:32:28 +0700

    flutter_firebase

--- README.md
+++ README.md
@@ -1,16 +1,70 @@
-# flutter_firebase
+# **Tugas Pertemuan 29**
+## **Flutter_firebase**
 
-A new Flutter project.
+ 
+### Langkah 1 : Impor Aplikasi Pemula
+* Kloning repositori GitHub dari baris perintah
+![Screenshot flutter_firebase](images/01.png)
 
-## Getting Started
+* Mengimport File dari folder firebase-get-to-know/step_02 dengan mengimput perintah
+![Screenshot flutter_firebase](images/02.png)
+* Meninjau file lib/main.dart
+![Screenshot flutter_firebase](images/03.png)
+![Screenshot flutter_firebase](images/04.png)
 
-This project is a starting point for a Flutter application.
+* Menambahkan package google_fonts serta folder assets pada pubspec.yaml
+![Screenshot flutter_firebase](images/05.png)
+![Screenshot flutter_firebase](images/06.png)
 
-A few resources to get you started if this is your first Flutter project:
+* Hasil Sementara
+![Screenshot flutter_firebase](images/07.png)
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+### Langkah 2 : Membuat proyek firebase
+* Membuat project pada firebase
+![Screenshot flutter_firebase](images/08.png)
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+
+### Langkah 3 : Konfigurasi dependensi
+* menambahkan pustaka FlutterFire untuk dua produk Firebase yang Anda gunakan di aplikasi
+![Screenshot flutter_firebase](images/09.png)
+
+* Memasang flutterfire serta mengonfigurasi aplikasi
+![Screenshot flutter_firebase](images/10.png)
+![Screenshot flutter_firebase](images/11.png)
+
+
+### Langkah 4 : Tambah Login Pengguna RSVP
+* Menyiapkan tombol RSVP dengan mengedit file main.dart
+![Screenshot flutter_firebase](images/12.png)
+
+* Hasil yang ditampilkan
+![Screenshot flutter_firebase](images/13.png)
+![Screenshot flutter_firebase](images/14.png)
+![Screenshot flutter_firebase](images/15.png)
+![Screenshot flutter_firebase](images/16.png)
+
+
+### Langkah 5 : Tulis pesan ke Cloud Firestore
+* menyimpan setiap pesan obrolan sebagai dokumen dalam koleksi tingkat atas yang disebut guestbook
+![Screenshot flutter_firebase](images/17.png)
+![Screenshot flutter_firebase](images/18.png)
+![Screenshot flutter_firebase](images/19.png)
+
+
+### Langkah 6 : Baca Pesan
+* Menampilkan pesan
+![Screenshot flutter_firebase](images/20.png)
+
+
+### Langkah 7 : Siapkan aturan kemanan dasar
+![Screenshot flutter_firebase](images/21.png)
+
+
+### Langkah 8 : Langkah bonus
+* Mencatat status RSVP peserta
+![Screenshot flutter_firebase](images/22.png)
+![Screenshot flutter_firebase](images/23.png)
+
+* Menambahkan Aturan Validasi pada firebase
+![Screenshot flutter_firebase](images/24.png)
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,21 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
-import 'dart:async'; // new
-import 'package:cloud_firestore/cloud_firestore.dart'; // new
-
-import 'package:firebase_auth/firebase_auth.dart'; // new
-import 'package:firebase_core/firebase_core.dart'; // new
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart'; // new
-import 'firebase_options.dart'; // new
+import 'firebase_options.dart';
 import 'src/authentication.dart';
-// new
 import 'src/widgets.dart';
 
 void main() {
   runApp(
     ChangeNotifierProvider(
       create: (context) => ApplicationState(),
-      builder: (context, _) => App(),
+      builder: (context, _) => const App(),
     ),
   );
 }
@@ -58,8 +55,8 @@ class HomePage extends StatelessWidget {
         children: <Widget>[
           Image.asset('assets/codelab.png'),
           const SizedBox(height: 8),
-          const IconAndDetail(Icons.calendar_today, 'September 15'),
-          const IconAndDetail(Icons.location_city, 'Malang'),
+          const IconAndDetail(Icons.calendar_today, 'October 30'),
+          const IconAndDetail(Icons.location_city, 'San Francisco'),
           Consumer<ApplicationState>(
             builder: (context, appState, _) => Authentication(
               email: appState.email,
@@ -72,7 +69,6 @@ class HomePage extends StatelessWidget {
               signOut: appState.signOut,
             ),
           ),
-
           const Divider(
             height: 8,
             thickness: 1,
@@ -84,18 +80,26 @@ class HomePage extends StatelessWidget {
           const Paragraph(
             'Join us for a day full of Firebase Workshops and Pizza!',
           ),
-          // const Header('Discussion'),
-          // GuestBook(addMessage: (message) => print(message)),
           Consumer<ApplicationState>(
             builder: (context, appState, _) => Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                if (appState.attendees >= 2)
+                  Paragraph('${appState.attendees} people going')
+                else if (appState.attendees == 1)
+                  const Paragraph('1 person going')
+                else
+                  const Paragraph('No one going'),
                 if (appState.loginState == ApplicationLoginState.loggedIn) ...[
+                  YesNoSelection(
+                    state: appState.attending,
+                    onSelection: (attending) => appState.attending = attending,
+                  ),
                   const Header('Discussion'),
                   GuestBook(
                     addMessage: (message) =>
                         appState.addMessageToGuestBook(message),
-                    messages: appState.guestBookMessages, // new
+                    messages: appState.guestBookMessages,
                   ),
                 ],
               ],
@@ -108,21 +112,6 @@ class HomePage extends StatelessWidget {
 }
 
 class ApplicationState extends ChangeNotifier {
-  Future<DocumentReference> addMessageToGuestBook(String message) {
-    if (_loginState != ApplicationLoginState.loggedIn) {
-      throw Exception('Must be logged in');
-    }
-
-    return FirebaseFirestore.instance
-        .collection('guestbook')
-        .add(<String, dynamic>{
-      'text': message,
-      'timestamp': DateTime.now().millisecondsSinceEpoch,
-      'name': FirebaseAuth.instance.currentUser!.displayName,
-      'userId': FirebaseAuth.instance.currentUser!.uid,
-    });
-  }
-
   ApplicationState() {
     init();
   }
@@ -132,10 +121,18 @@ class ApplicationState extends ChangeNotifier {
       options: DefaultFirebaseOptions.currentPlatform,
     );
 
+    FirebaseFirestore.instance
+        .collection('attendees')
+        .where('attending', isEqualTo: true)
+        .snapshots()
+        .listen((snapshot) {
+      _attendees = snapshot.docs.length;
+      notifyListeners();
+    });
+
     FirebaseAuth.instance.userChanges().listen((user) {
       if (user != null) {
         _loginState = ApplicationLoginState.loggedIn;
-
         _guestBookSubscription = FirebaseFirestore.instance
             .collection('guestbook')
             .orderBy('timestamp', descending: true)
@@ -152,11 +149,27 @@ class ApplicationState extends ChangeNotifier {
           }
           notifyListeners();
         });
+        _attendingSubscription = FirebaseFirestore.instance
+            .collection('attendees')
+            .doc(user.uid)
+            .snapshots()
+            .listen((snapshot) {
+          if (snapshot.data() != null) {
+            if (snapshot.data()!['attending'] as bool) {
+              _attending = Attending.yes;
+            } else {
+              _attending = Attending.no;
+            }
+          } else {
+            _attending = Attending.unknown;
+          }
+          notifyListeners();
+        });
       } else {
         _loginState = ApplicationLoginState.loggedOut;
-
         _guestBookMessages = [];
         _guestBookSubscription?.cancel();
+        _attendingSubscription?.cancel();
       }
       notifyListeners();
     });
@@ -171,6 +184,23 @@ class ApplicationState extends ChangeNotifier {
   StreamSubscription<QuerySnapshot>? _guestBookSubscription;
   List<GuestBookMessage> _guestBookMessages = [];
   List<GuestBookMessage> get guestBookMessages => _guestBookMessages;
+
+  int _attendees = 0;
+  int get attendees => _attendees;
+
+  Attending _attending = Attending.unknown;
+  StreamSubscription<DocumentSnapshot>? _attendingSubscription;
+  Attending get attending => _attending;
+  set attending(Attending attending) {
+    final userDoc = FirebaseFirestore.instance
+        .collection('attendees')
+        .doc(FirebaseAuth.instance.currentUser!.uid);
+    if (attending == Attending.yes) {
+      userDoc.set(<String, dynamic>{'attending': true});
+    } else {
+      userDoc.set(<String, dynamic>{'attending': false});
+    }
+  }
 
   void startLoginFlow() {
     _loginState = ApplicationLoginState.emailAddress;
@@ -233,15 +263,42 @@ class ApplicationState extends ChangeNotifier {
   void signOut() {
     FirebaseAuth.instance.signOut();
   }
+
+  Future<DocumentReference> addMessageToGuestBook(String message) {
+    if (_loginState != ApplicationLoginState.loggedIn) {
+      throw Exception('Must be logged in');
+    }
+
+    return FirebaseFirestore.instance
+        .collection('guestbook')
+        .add(<String, dynamic>{
+      'text': message,
+      'timestamp': DateTime.now().millisecondsSinceEpoch,
+      'name': FirebaseAuth.instance.currentUser!.displayName,
+      'userId': FirebaseAuth.instance.currentUser!.uid,
+    });
+  }
 }
 
+class GuestBookMessage {
+  GuestBookMessage({required this.name, required this.message});
+  final String name;
+  final String message;
+}
+
+enum Attending { yes, no, unknown }
+
 class GuestBook extends StatefulWidget {
-  const GuestBook({required this.addMessage, required this.messages});
+  const GuestBook({
+    required this.addMessage,
+    required this.messages,
+    super.key,
+  });
   final FutureOr<void> Function(String message) addMessage;
-  final List<GuestBookMessage> messages; // new
+  final List<GuestBookMessage> messages;
 
   @override
-  _GuestBookState createState() => _GuestBookState();
+  State<GuestBook> createState() => _GuestBookState();
 }
 
 class _GuestBookState extends State<GuestBook> {
@@ -302,8 +359,71 @@ class _GuestBookState extends State<GuestBook> {
   }
 }
 
-class GuestBookMessage {
-  GuestBookMessage({required this.name, required this.message});
-  final String name;
-  final String message;
+class YesNoSelection extends StatelessWidget {
+  const YesNoSelection({
+    required this.state,
+    required this.onSelection,
+    super.key,
+  });
+  final Attending state;
+  final void Function(Attending selection) onSelection;
+
+  @override
+  Widget build(BuildContext context) {
+    switch (state) {
+      case Attending.yes:
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(elevation: 0),
+                onPressed: () => onSelection(Attending.yes),
+                child: const Text('YES'),
+              ),
+              const SizedBox(width: 8),
+              TextButton(
+                onPressed: () => onSelection(Attending.no),
+                child: const Text('NO'),
+              ),
+            ],
+          ),
+        );
+      case Attending.no:
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              TextButton(
+                onPressed: () => onSelection(Attending.yes),
+                child: const Text('YES'),
+              ),
+              const SizedBox(width: 8),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(elevation: 0),
+                onPressed: () => onSelection(Attending.no),
+                child: const Text('NO'),
+              ),
+            ],
+          ),
+        );
+      default:
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              StyledButton(
+                onPressed: () => onSelection(Attending.yes),
+                child: const Text('YES'),
+              ),
+              const SizedBox(width: 8),
+              StyledButton(
+                onPressed: () => onSelection(Attending.no),
+                child: const Text('NO'),
+              ),
+            ],
+          ),
+        );
+    }
+  }
 }
